a3129d5f62adc05f4b8f1003036284cbf8400d5d
Gustavo Noronha Silva <gustavo.noronha@collabora.com>
2020-10-10 17:17:46 -0300

    first 2 sorted tests

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,11 +1,20 @@
+#![feature(proc_macro_diagnostic)]
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+#[macro_use]
+extern crate syn;
+
+use proc_macro::{TokenStream};
+use proc_macro2::Span;
+use syn::{Error, Item};
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
-
-    unimplemented!()
+pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let result = input.clone();
+    let item = parse_macro_input!(input as Item);
+    match item {
+        Item::Enum(_) => (),
+        _ => return Error::new(Span::call_site(), "expected enum or match expression").to_compile_error().into(),
+    }
+    result
 }
